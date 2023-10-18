72e597c836600e9ee468d4ee2c214b03d4147e3f
Lorenzo Hermoso Moreno <lmhm.yeah@gmail.com>
2023-10-18 20:32:14 +0200

    header and started experience

---
+++ src/components/experience.tsx
@@ -0,0 +1,11 @@
+export default function Experience() {
+    return (
+        <section className="w-full min-h-screen">
+            <div className="flex items-center">
+                <div className="h-[1px] w-[100px] bg-white" />
+                <p className="text-xl mx-2">Experience</p>
+                <div className="h-[1px] flex-grow bg-white" />
+            </div>
+        </section>
+    )
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <section className="w-full min-h-screen flex flex-col items-center justify-center">
+            <h1 className="text-7xl font-semibold text-center">Lorenzo Hermoso</h1>
+            <p className="text-2xl text-blue-400 font-thin">Software Developer</p>
+        </section>
+    )
+}

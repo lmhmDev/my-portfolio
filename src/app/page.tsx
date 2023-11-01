b1a453853e36f2639f1f4a50cb2630868ca0313e
Lorenzo Hermoso Moreno <lmhm.yeah@gmail.com>
2023-11-01 21:15:09 +0100

    header name and short description and links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
             <Header />
             <main className='pt-24 lg:w-1/2 lg:py-24'>
                 <Experience />
-                <Projects />
+                {/* <Projects /> */}
             </main>
         </div>
     )
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,19 @@
 const Header = () => {
-  return (
-    <header className='lg:sticky lg:top-0 lg:max-h-screen lg:w-1/2 flex min-h-screen w-full flex-col items-center justify-center'>
-      <h1 className='text-center text-7xl font-semibold'>Lorenzo Hermoso</h1>
-      <p className='text-2xl font-thin text-blue-400'>Software Developer</p>
-    </header>
-  )
+	return (
+		<header className='lg:sticky lg:top-0 lg:max-h-screen lg:w-1/2 flex min-h-screen w-full flex-col pt-24 lg:w-1/2 lg:py-24 justify-between'>
+			<section>
+				<h1 className='text-5xl font-bold'>Lorenzo Hermoso</h1>
+				<p className='text-2xl mt-2  text-blue-400'>Software Developer</p>
+				<p className='font-light opacity-75 mt-6'>
+					I enjoy building stuff that lives on the internet.<br />Passionate about tech and web development.
+				</p>
+			</section>
+			<section className='flex gap-2'>
+				<a href="" className='opacity-50 hover:opacity-100'>Github</a>
+				<a href="" className='opacity-50 hover:opacity-100'>Linkedin</a>
+			</section>
+		</header>
+	)
 }
 
 export default Header

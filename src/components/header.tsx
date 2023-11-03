a1138bf87a8b738c78531457f3105139618dc536
Lorenzo Hermoso Moreno <lmhm.yeah@gmail.com>
2023-11-03 12:42:21 +0100

    header responsive

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,15 +3,15 @@ import LinkedinIcon from "./icons/linkedin-icon"
 
 const Header = () => {
 	return (
-		<header className='lg:sticky lg:top-0 lg:max-h-screen lg:w-1/2 flex min-h-screen w-full flex-col pt-24 lg:w-1/2 lg:py-24 justify-between'>
-			<section>
+		<header className='lg:sticky lg:top-0 lg:max-h-screen lg:w-1/2 flex lg:min-h-screen w-full flex-col pt-6 lg:pt-24 lg:w-1/2 lg:py-24 justify-between'>
+			<section className='mb-14'>
 				<h1 className='text-5xl font-bold'>Lorenzo Hermoso</h1>
 				<p className='text-2xl mt-2  text-blue-400'>Software Developer</p>
 				<p className='font-light opacity-75 mt-6'>
 					I enjoy building stuff that lives on the internet.<br />Passionate about tech and web development.
 				</p>
 			</section>
-			<section className='flex flex-col gap-4'>
+			<section className='flex flex-col gap-4 hidden lg:block'>
 				<a href='' className='group w-fit opacity-50 hover:opacity-100 flex items-center text-sm tracking-widest font-semibold'>
 					<div className='group-hover:w-[5rem] transition-[1s] w-[3rem] h-[1px] bg-white rounded-sm mr-2' />
 					ABOUT

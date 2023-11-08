5c99dbdfa591b4c3b531f43748a1b0e5f070fe05
Lorenzo Hermoso Moreno <lmhm.yeah@gmail.com>
2023-11-08 18:58:02 +0100

    section titles for mobile

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -3,8 +3,9 @@ import JobCard from './job-card'
 
 export default function Experience() {
     return (
-        <section className='min-h-screen w-full'>
+        <section className='min-h-fit mb-16 lg:mb-24 w-full'>
             <div className='flex flex-col items-start justify-center'>
+                <p className='mb-10 lg:hidden text-xl'>Experience</p>
                 {jobs.map((job) => {
                     return <JobCard key={job.company} job={job} />
                 })}

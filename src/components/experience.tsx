d253765856c1fd7fe1c023e29c291926861cdf34
Lorenzo Hermoso Moreno <lmhm.yeah@gmail.com>
2023-11-13 12:03:44 +0100

    experience cards hover effect

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -10,17 +10,18 @@ const Experience: React.ForwardRefRenderFunction<HTMLElement, Props> = (props, r
 
     return (
         <section
-            className='mb-16 min-h-fit w-full scroll-mt-16 lg:mb-24'
+            className='mb-16 min-h-fit w-full scroll-mt-24 lg:mb-24'
             id='experience'
             ref={ref}
             onMouseLeave={() => setActiveCard(null)}
         >
             <div className='flex flex-col items-start justify-center'>
                 <p className='mb-10 text-xl lg:hidden'>Experience</p>
-                {jobs.map((job) => {
-                    const isActive = activeCard ? activeCard == job.company ? true : false : null
-                    return <JobCard key={job.company} job={job} isActive={isActive} setActive={setActiveCard} />
-                })}
+                <div className='group/list'>
+                    {jobs.map((job) => {
+                        return <JobCard key={job.company} job={job} />
+                    })}
+                </div>
             </div>
         </section>
     )
--- src/components/job-card.tsx
+++ src/components/job-card.tsx
@@ -3,17 +3,16 @@ import TechTag from './technologyTag'
 
 interface Props {
     job: job
-    isActive: boolean | null
-    setActive: (active: string) => void
 }
 
-const JobCard = ({ job, isActive, setActive }: Props) => {
+const JobCard = ({ job }: Props) => {
     return (
-        <article onMouseEnter={() => setActive(job.company)} className={`mb-12 flex w-full flex-col gap-2 rounded-sm lg:flex-row p-3 transition ${isActive !== null ? isActive ? ' bg-gray-400 rounded-md backdrop-filter backdrop-blur-3xl bg-opacity-10' : 'opacity-50' : ''}`}>
-            <p className='mb-1 mt-[0.22rem] min-w-[25%] text-xs font-medium uppercase tracking-wide text-blue-400 opacity-75 lg:mb-0'>
+        <article className='group relative mb-12 flex w-full flex-col gap-2 rounded-sm lg:flex-row p-3 transition lg:hover:!opacity-100 lg:group-hover/list:opacity-50 cursor-pointer'>
+            <div className='absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-700/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg' />
+            <p className='z-10 mb-1 mt-[0.22rem] min-w-[25%] text-xs font-medium uppercase tracking-wide text-blue-400 opacity-75 lg:mb-0'>
                 {job.date}
             </p>
-            <div className='lg:w-[75%]'>
+            <div className='lg:w-[75%] z-10'>
                 <p className='leading-tight text-slate-200'>
                     {job.position} · {job.company}
                 </p>

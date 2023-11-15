92596d5d4bd0ce45a0400feb74e8dbbd3590558c
Lorenzo Hermoso Moreno <lmhm.yeah@gmail.com>
2023-11-15 19:49:22 +0100

    fix visual bugs

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -14,7 +14,7 @@ const Header = ({ activeSection }: Props) => {
 
     return (
         <header className='flex w-full flex-col justify-between pt-6 lg:sticky lg:top-0 lg:max-h-screen lg:min-h-screen lg:w-1/2 lg:w-1/2 lg:py-24 lg:pt-24'>
-            <section className='mb-14'>
+            <section className='mb-14 z-10'>
                 <h1 className='text-5xl font-bold'>Lorenzo Hermoso</h1>
                 <p className='mt-2 text-2xl  text-blue-400'>
                     Software Developer
@@ -27,52 +27,46 @@ const Header = ({ activeSection }: Props) => {
                 <div className='mt-16 flex hidden flex-col gap-4 lg:flex'>
                     <a
                         href='/#about'
-                        className={`group flex w-fit items-center text-sm font-semibold tracking-widest ${
-                            activeSection === sections.about
-                                ? 'opacity-100'
-                                : 'opacity-50'
-                        } hover:opacity-100`}
+                        className={`group flex w-fit items-center text-sm font-semibold tracking-widest ${activeSection === sections.about
+                            ? 'opacity-100'
+                            : 'opacity-50'
+                            } hover:opacity-100`}
                     >
                         <div
-                            className={`mr-2 h-[1px] rounded-sm bg-white transition-[1s] ${
-                                activeSection === sections.about
-                                    ? 'w-[5rem]'
-                                    : 'w-[3rem]'
-                            } group-hover:w-[5rem]`}
+                            className={`mr-2 h-[1px] rounded-sm bg-white transition-[1s] ${activeSection === sections.about
+                                ? 'w-[5rem]'
+                                : 'w-[3rem]'
+                                } group-hover:w-[5rem]`}
                         />
                         ABOUT
                     </a>
                     <a
                         href='/#experience'
-                        className={`group flex w-fit items-center text-sm font-semibold tracking-widest ${
-                            activeSection === sections.experience
-                                ? 'opacity-100'
-                                : 'opacity-50'
-                        } hover:opacity-100`}
+                        className={`group flex w-fit items-center text-sm font-semibold tracking-widest ${activeSection === sections.experience
+                            ? 'opacity-100'
+                            : 'opacity-50'
+                            } hover:opacity-100`}
                     >
                         <div
-                            className={`mr-2 h-[1px]  rounded-sm bg-white transition-[1s] ${
-                                activeSection === sections.experience
-                                    ? 'w-[5rem]'
-                                    : 'w-[3rem]'
-                            } group-hover:w-[5rem]`}
+                            className={`mr-2 h-[1px]  rounded-sm bg-white transition-[1s] ${activeSection === sections.experience
+                                ? 'w-[5rem]'
+                                : 'w-[3rem]'
+                                } group-hover:w-[5rem]`}
                         />
                         EXPERIENCE
                     </a>
                     <a
                         href='/#projects'
-                        className={`group flex w-fit items-center text-sm font-semibold tracking-widest ${
-                            activeSection === sections.projects
-                                ? 'opacity-100'
-                                : 'opacity-50'
-                        } hover:opacity-100`}
+                        className={`group flex w-fit items-center text-sm font-semibold tracking-widest ${activeSection === sections.projects
+                            ? 'opacity-100'
+                            : 'opacity-50'
+                            } hover:opacity-100`}
                     >
                         <div
-                            className={`mr-2 h-[1px] rounded-sm bg-white transition-[1s] ${
-                                activeSection === sections.projects
-                                    ? 'w-[5rem]'
-                                    : 'w-[3rem]'
-                            } group-hover:w-[5rem]`}
+                            className={`mr-2 h-[1px] rounded-sm bg-white transition-[1s] ${activeSection === sections.projects
+                                ? 'w-[5rem]'
+                                : 'w-[3rem]'
+                                } group-hover:w-[5rem]`}
                         />
                         PROJECTS
                     </a>

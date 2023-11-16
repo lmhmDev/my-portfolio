d42d5eab58758d660c03f3c364a80c7f195c2336
Lorenzo Hermoso Moreno <lmhm.yeah@gmail.com>
2023-11-16 11:57:11 +0100

    commented tell me more project

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -10,14 +10,14 @@ export const projects: Project[] = [
             'Task it is a kanban board that helps you organize your todos between different columns using drag and drop.',
         technologies: [ReactTech, NextTech, TypescriptTech, TailwindTech],
     },
-    {
+    /* {
         url: 'https://tell-me-more-eta.vercel.app/',
         imgUrl: '/img/tell-me-more.png',
         name: 'Tell me more',
         description:
             'Web App that tells you a fun fact about anything you can describe in 2 words or less.',
         technologies: [ReactTech, NextTech, TypescriptTech, TailwindTech],
-    },
+    }, */
     {
         url: 'https://paas-three.vercel.app/',
         imgUrl: '/img/paas.png',
